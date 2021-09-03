a8568ad50c373fcec692251403b159f195b1f0aa
Christian Altamirano <christian.altamirano.ayala@gmail.com>
2021-09-03 15:41:18 -0500

    export environment variable
    cluster base url and jwt header

--- src/app/services/api-group.service.ts
+++ src/app/services/api-group.service.ts
@@ -1,15 +1,12 @@
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 import { ApiGroupList } from '../model/api-group-list';
 import { ApiResourceList } from '../model/api-resource-list';
 
-
-const API_BASE='https://192.168.0.110:6443'
-
-const headers = new HttpHeaders()
-            .set("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ik1oYV9kVmVuT2lBR2VKNndJSzFxQ1BESENsWDVPTUhyS0tiZ1VKbWwxS3MifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImFwaS11c2VyLXRva2VuLTZtY2tjIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImFwaS11c2VyIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiODVhZjRkN2QtNjMyZC00OTE3LThiMzItMWZiNWUyMDA0ZWM2Iiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50OmRlZmF1bHQ6YXBpLXVzZXIifQ.ZYR6y1d8We91qUhAbZdiS03TRiewXSC6Q5ZtEajKfwiGGtKAaqiF-EQuPkMMHrjW1scvulpIAI0vshED37abhFYENhj1Z0rN7HTtWJYpq2ui8II_WIm3gFXour0QNbh5lUQ-hF2Gu_2zkvGiink1xIlhrnrYo971gRSNhJDkjapmFR_s4CixqTQswrCyzRddrQ_aLX_RZ3CiuGYR8GKpmsTG7_ytAOPvcoaNEP9mE7ZZngs2BPR4xZJkVIru2Gx834ntSsg64HmoLn__ttcJqgPaK7Fc71RgIc7ikhxW0LRWV-EjEo2TogzQzYA1y1oTadFGFwKlnB1zZEVVUWKBYQ")
-            .set("Access-Control-Allow-Origin", "*");
+const API_BASE=environment.API_BASE
+const headers = environment.headers
 
 @Injectable({
   providedIn: 'root'
